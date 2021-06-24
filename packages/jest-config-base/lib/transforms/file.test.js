d4904f2bcce736280b0041b1248adbe7ee08f3e1
André Cruz <andremiguelcruz@msn.com>
2021-06-24 14:44:51 +0100

    feat(jest-config-base): add json5 support

--- packages/jest-config-base/lib/transforms/file.test.js
+++ packages/jest-config-base/lib/transforms/file.test.js
@@ -12,12 +12,12 @@ afterAll(() => {
 
 it('should return basename of the file', () => {
     const contents = 'foo';
-    const filename = `${dir}/inline-test-file.txt`;
+    const filename = `${dir}/test-file.txt`;
 
     fs.writeFileSync(filename, contents);
 
     const output = inlineTransformer.process(contents, filename, {});
 
-    expect(output).toContain('inline-test-file.txt');
+    expect(output).toContain('test-file.txt');
     expect(output).toMatchSnapshot();
 });
---
+++ packages/jest-config-base/lib/transforms/json5.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const json5 = require('json5');
+
+module.exports = {
+    process: (src) => `module.exports = ${JSON.stringify(json5.parse(src))};`,
+};
